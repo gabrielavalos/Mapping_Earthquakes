cbb695bddfa46c2c9cc961a9cc170af8996c505f
Gabriela E Avalos <gabrielaeavalos@gabrielas-air.home>
2021-03-31 23:34:06 -0500

    yellow circle added

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,39 +14,8 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 streets.addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
-// An array containing each city's location, state, and population.
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-  },
-  {
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-  },
-  {
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-  },
-  {
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-  },
-  {
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-  }
-  ];
-
-  cities.forEach(function(city) {
-      console.log(city)
-  });
+var marker = L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: 'black',
+  fillColor: '#ffffa1'
+}).addTo(map);
